90be1f629d087648f5517a70101dbb0904b03da3
Reysmer Valle <reysmerwvr@gmail.com>
2020-08-12 13:27:42 -0500

    Updates

---
+++ precache-manifest.a04d5637527c451a4cea5a42f9d84384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e78a0207d5d1deac5163c254bb432ad",
+    "url": "/ml-frontend-test/index.html"
+  },
+  {
+    "revision": "da0c1a29bd73eff52511",
+    "url": "/ml-frontend-test/static/css/main.f845a2d8.chunk.css"
+  },
+  {
+    "revision": "f401497cdca03fe43b06",
+    "url": "/ml-frontend-test/static/js/2.fafd6eee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ml-frontend-test/static/js/2.fafd6eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0c1a29bd73eff52511",
+    "url": "/ml-frontend-test/static/js/main.3cbdf57d.chunk.js"
+  },
+  {
+    "revision": "206372789891e85ddc4d",
+    "url": "/ml-frontend-test/static/js/runtime-main.93b40ac5.js"
+  }
+]);
